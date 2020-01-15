0f7f3fd2f2ee4864e6fef5cff421d213b821b611
devincloudkelly <devin.cloud.kelly@gmail.com>
2020-01-15 11:13:45 -0800

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import { actors } from '../data';
+import { act } from 'react-test-renderer';
 
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map(actor => {
+        return <div>
+          {actor.name}<br/>
+          Movies: {actor.movies.map(movie => {
+            return <ul>{movie}</ul>
+          })}
+        </div>
+      })}
     </div>
   );
 };
